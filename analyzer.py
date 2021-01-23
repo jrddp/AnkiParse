0ea0a847fd0d9e03489fdd0d2289af3d114ba7ed
Jared P <jar3dpt@gmail.com>
2021-01-23 05:07:49 -0500

    Create dedicated analyzer file

---
+++ analyzer.py
@@ -0,0 +1,23 @@
+import re
+from commands import Command
+
+command_pattern = "!(\w)(\w*)\s*(.*)"
+
+with open("test/anki_test.txt", "r") as f:
+        lines = f.readlines()
+
+
+current_command = None
+
+def start_command(command):
+    global current_command
+    if current_command: current_command.do()
+    current_command = command if not command.done else None
+
+for line in lines:
+    if len(line) > 0 and line.startswith("!"):
+        action, args, body = re.match(command_pattern, line).groups()
+        start_command(Command.create(action, args, body))
+    elif current_command:
+        current_command += line
+if current_command: current_command.do()
