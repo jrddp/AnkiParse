0e81c2e8ecea99112f6eb293fe9384cb2b64d83f
Jared P <jar3dpt@gmail.com>
2021-01-23 07:08:46 -0500

    Extract into file analysis method

--- analyzer.py
+++ analyzer.py
@@ -3,21 +3,18 @@ from commands import Command
 
 command_pattern = "!(\S)(\S*)\s*(.*)"
 
-with open("test/anki_test.txt", "r") as f:
-        lines = f.readlines()
+def analyze(file):
+    current_command = None
 
+    def start_command(command):
+        nonlocal current_command
+        if current_command: current_command.do()
+        current_command = command if not command.done else None
 
-current_command = None
-
-def start_command(command):
-    global current_command
+    for line in file.readlines():
+        if len(line) > 0 and line.startswith("!"):
+            action, args, body = re.match(command_pattern, line).groups()
+            start_command(Command.create(action, args, body))
+        elif current_command:
+            current_command += line
     if current_command: current_command.do()
-    current_command = command if not command.done else None
-
-for line in lines:
-    if len(line) > 0 and line.startswith("!"):
-        action, args, body = re.match(command_pattern, line).groups()
-        start_command(Command.create(action, args, body))
-    elif current_command:
-        current_command += line
-if current_command: current_command.do()
