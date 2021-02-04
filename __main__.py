65cf41bedc296674ecdc30225fb558cc95c67a6e
Jared P <jar3dpt@gmail.com>
2021-02-04 13:32:07 -0500

    Add in-file reformatting

--- __main__.py
+++ __main__.py
@@ -4,3 +4,7 @@ if __name__ == '__main__':
 
     path = Path(sys.argv[1])
     analyzer.analyze(path)
+    with open(path, "r") as f:
+        lines = analyzer.replace_with_note_formatting(f.readlines())
+    with open(path, "w") as f:
+        f.writelines(lines)
