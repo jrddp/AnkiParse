148f0a4e48e43445c903dd9f309f45c553137c77
Jared P <jar3dpt@gmail.com>
2021-02-27 16:05:51 -0500

    Create dedicated file for using AnkiConnect

---
+++ anki_connector.py
@@ -0,0 +1,43 @@
+# Gateway for using the Anki-Connect addon and interacting with the user's Anki profile
+
+anki_connect_port = 'http://127.0.0.1:8765'
+
+
+def get_image_store_json(image_path):
+    import formatter, analyzer
+    is_url = formatter.is_url(image_path)
+    if not is_url:
+        image_path = analyzer.current_file_path.absolute().parent / image_path
+    return {
+        "action": "storeMediaFile",
+        "version": 6,
+        "params": {
+            "filename": formatter.get_image_name(image_path),
+            "url" if is_url else "path": str(image_path)
+        }
+    }
+
+
+def send_card_to_anki(card):
+    import requests
+
+    if card.image_paths:
+        actions = [get_image_store_json(p) for p in card.image_paths]
+        actions.append(card.get_json())
+
+        r = requests.post(anki_connect_port, json={
+            "action": "multi",
+            "params": {
+                "actions": actions
+            }
+        })
+    else:
+        r = requests.post(anki_connect_port, json=card.get_json())
+
+    # response can either be list or dict
+    response = r.json()
+    errors = [response['error']] if isinstance(r.json(), dict) else [r['error'] for r in response]
+    if errors:
+        for err in errors:
+            if err is not None: raise RuntimeError(f"AnkiConnect: {err}")
+    print(f"Uploaded card to {card.deck}")
--- cards.py
+++ cards.py
@@ -1,6 +1,5 @@
 import formatter
-
-anki_connect_port = 'http://127.0.0.1:8765'
+from anki_connector import send_card_to_anki
 
 
 class Card:
@@ -79,46 +78,6 @@ class Card:
             Card.closed_cards.append(self)
         except Exception as e:
             print(f"{e.__class__.__name__}: {e.args}")
-
-
-def get_image_store_json(image_path):
-    import formatter, analyzer
-    is_url = formatter.is_url(image_path)
-    if not is_url:
-        image_path = analyzer.current_file_path.absolute().parent / image_path
-    return {
-        "action": "storeMediaFile",
-        "version": 6,
-        "params": {
-            "filename": formatter.get_image_name(image_path),
-            "url" if is_url else "path": str(image_path)
-        }
-    }
-
-
-def send_card_to_anki(card):
-    import requests
-
-    if card.image_paths:
-        actions = [get_image_store_json(p) for p in card.image_paths]
-        actions.append(card.get_json())
-
-        r = requests.post(anki_connect_port, json={
-            "action": "multi",
-            "params": {
-                "actions": actions
-            }
-        })
-    else:
-        r = requests.post(anki_connect_port, json=card.get_json())
-
-    # response can either be list or dict
-    response = r.json()
-    errors = [response['error']] if isinstance(r.json(), dict) else [r['error'] for r in response]
-    if errors:
-        for err in errors:
-            if err is not None: raise RuntimeError(f"AnkiConnect: {err}")
-    print(f"Uploaded card to {card.deck}")
 
 
 current_deck = None
