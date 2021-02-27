e207b9437361c2e6e48e6298c8d7ab2cb65f701d
Jared P <jar3dpt@gmail.com>
2021-02-27 16:36:52 -0500

    Add argparse and in-file replacement as flag

--- __main__.py
+++ __main__.py
@@ -1,13 +1,22 @@
-# TODO make in-file replacement a flag option
 # TODO output total count of cards uploaded
 
 if __name__ == '__main__':
-    import analyzer, sys
+    import analyzer
     from pathlib import Path
+    from argparse import ArgumentParser
 
-    path = Path(sys.argv[1])
+    parser = ArgumentParser(description="Parse Anki Cards from Notes")
+    parser.add_argument("file", help="The file to parse cards from")
+    parser.add_argument("-r", "--reformat", help="Replace added card syntax in-file with Markdown formatting",
+                        action='store_true',
+                        dest="reformat")
+
+    args = parser.parse_args()
+
+    path = Path(args.file)
     analyzer.analyze(path)
     with open(path, "r") as f:
         lines = analyzer.replace_with_note_formatting(f.readlines())
-    with open(path, "w") as f:
-        f.writelines(lines)
+    if args.reformat:
+        with open(path, "w") as f:
+            f.writelines(lines)
